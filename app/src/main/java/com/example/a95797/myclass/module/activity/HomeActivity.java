76723cf2ea4bf085af21442ec12b5d32afd77250
957971829@qq.com <957971829@qq.com>
2017-12-30 09:31:58 +0800

    home layout

--- app/src/main/java/com/example/a95797/myclass/module/activity/HomeActivity.java
+++ app/src/main/java/com/example/a95797/myclass/module/activity/HomeActivity.java
@@ -2,13 +2,12 @@ package com.example.a95797.myclass.module.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.a95797.myclass.R;
-import com.example.a95797.myclass.adapter.MyFragementPagerAdapter;
 import com.example.a95797.myclass.module.fragment.HomeworkFragment;
 import com.example.a95797.myclass.module.fragment.OneselfFragment;
 import com.example.a95797.myclass.module.fragment.PreFragment;
@@ -34,7 +33,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.prebt.setSelected(true);
+
         initbt();
         initViewpager();
         this.prebt.setOnClickListener(this);
@@ -47,31 +46,34 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.pre:
+                Log.d("Button", "onClick1: ");
                 this.prebt.setSelected(true);
                 prebt.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.mipmap.pre), null, null);
                 this.homeworkbt.setSelected(false);
                 homeworkbt.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.mipmap.re_homework), null, null);
                 this.oneselfbt.setSelected(false);
                 homeworkbt.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.mipmap.re_oneself), null, null);
-                this.viewPager.setCurrentItem(0);
+//                this.viewPager.setCurrentItem(0);
                 break;
             case R.id.homework:
+                Log.d("Button", "onClick2: ");
                 this.prebt.setSelected(false);
                 prebt.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.mipmap.re_pre), null, null);
                 this.homeworkbt.setSelected(true);
                 homeworkbt.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.mipmap.homework), null, null);
                 this.oneselfbt.setSelected(false);
-                homeworkbt.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.mipmap.re_oneself), null, null);
-                this.viewPager.setCurrentItem(1);
+                oneselfbt.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.mipmap.re_oneself), null, null);
+//                this.viewPager.setCurrentItem(1);
                 break;
             case R.id.oneself:
+                Log.d("Button", "onClick3: ");
                 this.prebt.setSelected(false);
                 prebt.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.mipmap.re_pre), null, null);
                 this.homeworkbt.setSelected(false);
                 homeworkbt.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.mipmap.re_homework), null, null);
                 this.oneselfbt.setSelected(true);
-                homeworkbt.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.mipmap.oneself), null, null);
-                this.viewPager.setCurrentItem(2);
+                oneselfbt.setCompoundDrawablesWithIntrinsicBounds(null, getResources().getDrawable(R.mipmap.oneself), null, null);
+//                this.viewPager.setCurrentItem(2);
                 break;
             default:
                 break;
@@ -82,6 +84,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         this.prebt = (Button)findViewById(R.id.pre);
         this.homeworkbt = (Button)findViewById(R.id.homework);
         this.oneselfbt = (Button)findViewById(R.id.oneself);
+        this.prebt.setSelected(true);
     }
 
     private void initViewpager()
@@ -96,15 +99,15 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         this.titleList.add("课前预习");
         this.titleList.add("课后习题");
         this.titleList.add("自学拓展");
-        PagerAdapter localObject = new MyFragementPagerAdapter(getSupportFragmentManager(), this.titleList, this.fragmentList);
-        this.viewPager.setAdapter(localObject);
-        this.viewPager.setOffscreenPageLimit(2);
-        this.viewPager.setNoScroll(true);
+//        PagerAdapter localObject = new MyFragementPagerAdapter(getSupportFragmentManager(), this.titleList, this.fragmentList);
+//        this.viewPager.setAdapter(localObject);
+//        this.viewPager.setOffscreenPageLimit(2);
+//        this.viewPager.setNoScroll(true);
 
         this.prebt.setSelected(true);
         this.homeworkbt.setSelected(false);
         this.oneselfbt.setSelected(false);
-        this.viewPager.setCurrentItem(0);
+        //this.viewPager.setCurrentItem(0);
 
 
     }
